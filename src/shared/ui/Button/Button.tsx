89742a42b26a0a15401c401ea426ce43f175a00e
Шабанов Артём <artem-shabanov-2001@mail.ru>
2023-02-25 18:06:24 +0500

    eslint

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { ButtonHTMLAttributes, FC } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames'
-import styles from './Button.module.scss'
+import { classNames } from 'shared/lib/classNames/classNames';
+import styles from './Button.module.scss';
 
 export enum ThemeButton {
   CLEAR = 'clear',
@@ -10,12 +10,13 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: ThemeButton;
 }
-
 export const Button: FC<ButtonProps> = (props) => {
-  const { className, children, theme, ...otherProps } = props;
+  const {
+    className, children, theme, ...otherProps
+  } = props;
   return (
-    <button className={classNames(styles.Button, {}, [className, styles[theme]])} {...otherProps}>
+    <button type="button" className={classNames(styles.Button, {}, [className, styles[theme]])} {...otherProps}>
       {children}
     </button>
-  )
+  );
 };
