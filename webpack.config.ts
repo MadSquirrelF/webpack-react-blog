e8392b1f771be79112a37499df4d1a9f532f0184
ShabanooovArt <ShabanoooovArt@yandex.ru>
2023-12-08 04:27:12 +0500

    fix(css): fix css prod formater

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,5 @@
-import path from 'path';
-
 import webpack from 'webpack';
+import path from 'path';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 import { BuildEnv, BuildPaths } from './config/build/types/config';
 
@@ -13,10 +12,12 @@ export default (env: BuildEnv) => {
         locales: path.resolve(__dirname, 'public', 'locales'),
         buildLocales: path.resolve(__dirname, 'build', 'locales'),
     };
+
     const mode = env.mode || 'development';
-    const isDev = mode === 'development';
     const PORT = env.port || 3000;
     const apiUrl = env.apiUrl || 'http://localhost:8000';
+
+    const isDev = mode === 'development';
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
